cdea9b1a979d820eee2f14bb67b104dc378e470c
OrestTiny <orest.sd@gmail.com>
2021-10-12 23:54:39 +0300

    start work

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+
+
+    const link = $('.link');
+    link.text().replace('Products', ' ');
+
+    link.text().replace('Products', ' ')
+    console.log();
+
+    // const linkArr = link.text().split(' ');
+    // linkArr.pop();
+    // link.text(linkArr.join(' '))
+
+
+
+})
+
+
+
